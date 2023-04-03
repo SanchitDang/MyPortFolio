9e35ba06b0979c69bb5a2a58b3b21a5cbad52427
Sanchit <87980528+SanchitDang@users.noreply.github.com>
2023-04-04 00:12:38 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Contact from './components/Contact'
 import About from './components/About'
 import Projects from './components/Projects'
 import Achievements from './components/Achievements'
-import Pract from "./components/Pract";
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
           <Route path="/About" element={<About/>}/>
           <Route path="/Achievements" element={<Achievements/>}/>
           <Route path="/Contact" element={<Contact/>}/>
-          <Route path="/Pract" element={<Pract/>}/>
         </Routes>
     </BrowserRouter>
     
--- src/components/Home.js
+++ src/components/Home.js
@@ -12,10 +12,10 @@ export default function Home() {
     
     <div class=" dark-body container px-4 py-4 my-5 text-center ">
         <h1 class="display-5 fw-bold">Hi, I'm Sanchit Dang</h1>
-        <h1 class="display-8 fw-bold">Programmer & Web Developer</h1>
+        <h1 class="display-8 fw-bold">Programmer & Mobile App Developer</h1>
         <div class="col-lg-6 mx-auto py-2">
           <p class="lead mb-4 py-2">
-          Currently i am pursuing Btech from Dronacharya College Of Engineering specialization in AI/ML.
+          Currently I am pursuing Btech from Dronacharya College Of Engineering specialization in AI/ML.
           </p>
         </div>
 
@@ -28,7 +28,7 @@ export default function Home() {
               <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
             </svg></a>
           </li>
-          <li class="ms-3"><a class="px-4" href="https://linkedin.com/in/SanchitDang" target="_blank" rel="noopener noreferrer" >
+          <li class="ms-3"><a class="px-4" href="mailto:work.sanchitdang@gmail.com" target="_blank" rel="noopener noreferrer" >
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
               <path d="M8.941.435a2 2 0 0 0-1.882 0l-6 3.2A2 2 0 0 0 0 5.4v.314l6.709 3.932L8 8.928l1.291.718L16 5.714V5.4a2 2 0 0 0-1.059-1.765l-6-3.2ZM16 6.873l-5.693 3.337L16 13.372v-6.5Zm-.059 7.611L8 10.072.059 14.484A2 2 0 0 0 2 16h12a2 2 0 0 0 1.941-1.516ZM0 13.373l5.693-3.163L0 6.873v6.5Z" />
             </svg></a>
