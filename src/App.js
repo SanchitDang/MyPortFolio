0cbece3ed58b9393f43d901c529a9dfd28b8e1be
SanchitDang <work.sanchitdang@gmail.com>
2024-04-28 14:07:51 +0530

    chnaged routing, added firebase hosting

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
     
-    <BrowserRouter basename="/MyPortFolio">
+    <BrowserRouter basename="/">
         <Routes>
-          <Route path="/" element={<Home/>}/>    
-          <Route path="/Projects" element={<Projects/>}/>   
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Projects" element={<Projects/>}/>
           <Route path="/About" element={<About/>}/>
           <Route path="/Achievements" element={<Achievements/>}/>
           <Route path="/Contact" element={<Contact/>}/>
@@ -23,3 +23,7 @@ function App() {
 )}
 
 export default App;
+
+
+// npm run build
+// firebase deploy
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Apply dark theme class to body by default
+document.body.classList.add('dark-theme');
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
